12e07a1b64f65136d662eb90d1ee2e236f8222d9
flashfan88 <lavarjohnson20@gmail.com>
2019-08-15 13:00:36 -0500

    ugh!

---
+++ index.js
@@ -0,0 +1,19 @@
+//Problem 1
+//Ask the user to enter a word.
+// Keep asking them to enter a word until they enter the letter 'q'.
+
+let userInput = prompt("Enter a word");
+while(userInput !== 'q')
+{
+    alert("good job")
+}
+
+
+//Problem 2
+//Make any string array all lower case strings.
+//If you don't remember how to use that Google it.
+
+let array1 = ["one", "two", "three", "four", "five"];
+
+
+
